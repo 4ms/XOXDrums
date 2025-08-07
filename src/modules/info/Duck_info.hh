8e54df567f77bba9883a918fa1ef418abf2d60da
Ross Fish <ross@4mscompany.com>
2025-08-07 11:51:38 -0700

    Duck: moved button to top of XML list

--- src/modules/info/Duck_info.hh
+++ src/modules/info/Duck_info.hh
@@ -11,14 +11,14 @@ struct DuckInfo : ModuleInfoBase {
     static constexpr std::string_view description{"DuckingModule"};
     static constexpr uint32_t width_hp = 4;
     static constexpr std::string_view svg_filename{"res/Duck.svg"};
-    static constexpr std::string_view png_filename{"4ms-XOXDrums/Duck.png"};
+    static constexpr std::string_view png_filename{"4ms/Duck.png"};
 
     using enum Coords;
 
     static constexpr std::array<Element, 8> Elements{{
-		WhiteMomentary7mm{{to_mm<96>(38.4), to_mm<96>(238.638), Center, "Trigger", ""}},
 		Knob9mm{{to_mm<96>(38.4), to_mm<96>(62.174), Center, "Amount", ""}, 1.0f, 0.0, 100.0, "%"},
 		Knob9mm{{to_mm<96>(38.4), to_mm<96>(151.774), Center, "Time", ""}, 0.25f, 50.0, 2000.0, "ms"},
+		WhiteMomentary7mm{{to_mm<96>(38.4), to_mm<96>(238.638), Center, "Trigger", ""}},
 		AnalogJackInput4ms{{to_mm<96>(38.4), to_mm<96>(111.595), Center, "Amount CV In", ""}},
 		AnalogJackInput4ms{{to_mm<96>(38.4), to_mm<96>(203.44), Center, "Time CV In", ""}},
 		GateJackInput4ms{{to_mm<96>(38.4), to_mm<96>(277.519), Center, "Trigger In", ""}},
@@ -27,9 +27,9 @@ struct DuckInfo : ModuleInfoBase {
 }};
 
     enum class Elem {
-        TriggerButton,
         AmountKnob,
         TimeKnob,
+        TriggerButton,
         AmountCvIn,
         TimeCvIn,
         TriggerIn,
