f0b0dd25349e712dab85ce5f482555f59f7b6322
Ross Fish <ross@4mscompany.com>
2025-08-07 11:58:09 -0700

    Cowbell: moved button to top of XML list

--- src/modules/info/Cowbell_info.hh
+++ src/modules/info/Cowbell_info.hh
@@ -11,14 +11,14 @@ struct CowbellInfo : ModuleInfoBase {
     static constexpr std::string_view description{"Cowbell DrumModule"};
     static constexpr uint32_t width_hp = 4;
     static constexpr std::string_view svg_filename{"res/Cowbell.svg"};
-    static constexpr std::string_view png_filename{"4ms-XOXDrums/Cowbell.png"};
+    static constexpr std::string_view png_filename{"4ms/Cowbell.png"};
 
     using enum Coords;
 
     static constexpr std::array<Element, 7> Elements{{
-		WhiteMomentary7mm{{to_mm<96>(38.574), to_mm<96>(297.949), Center, "Trigger", ""}},
 		Knob9mm{{to_mm<96>(38.56), to_mm<96>(70.114), Center, "Pitch", ""}, 0.5f, 240.0, 540.0, "hz"},
 		Knob9mm{{to_mm<96>(38.56), to_mm<96>(198.622), Center, "Decay", ""}, 0.5f, 20.0, 120.0, "ms"},
+		WhiteMomentary7mm{{to_mm<96>(38.574), to_mm<96>(297.949), Center, "Trigger", ""}},
 		AnalogJackInput4ms{{to_mm<96>(38.56), to_mm<96>(134.368), Center, "Pitch CV In", ""}},
 		AnalogJackInput4ms{{to_mm<96>(38.574), to_mm<96>(262.875), Center, "Decay CV In", ""}},
 		GateJackInput4ms{{to_mm<96>(38.4), to_mm<96>(341.765), Center, "Trigger In", ""}},
@@ -26,9 +26,9 @@ struct CowbellInfo : ModuleInfoBase {
 }};
 
     enum class Elem {
-        TriggerButton,
         PitchKnob,
         DecayKnob,
+        TriggerButton,
         PitchCvIn,
         DecayCvIn,
         TriggerIn,
