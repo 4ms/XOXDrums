827299084dfe5dbc7eaa55c8b983e78df2061159
Ross Fish <ross@4mscompany.com>
2025-08-07 11:48:30 -0700

    Clave: moved button to top of XML list to stop enums from being messed up

--- src/modules/info/Clave_info.hh
+++ src/modules/info/Clave_info.hh
@@ -11,14 +11,14 @@ struct ClaveInfo : ModuleInfoBase {
     static constexpr std::string_view description{"Clave DrumModule"};
     static constexpr uint32_t width_hp = 4;
     static constexpr std::string_view svg_filename{"res/Clave.svg"};
-    static constexpr std::string_view png_filename{"4ms-XOXDrums/Clave.png"};
+    static constexpr std::string_view png_filename{"4ms/Clave.png"};
 
     using enum Coords;
 
     static constexpr std::array<Element, 7> Elements{{
-		WhiteMomentary7mm{{to_mm<96>(38.56), to_mm<96>(299.037), Center, "Trigger", ""}},
 		Knob9mm{{to_mm<96>(38.56), to_mm<96>(70.115), Center, "Pitch", ""}, 0.5f, 1.0, 2.0, "khz"},
 		Knob9mm{{to_mm<96>(38.56), to_mm<96>(198.622), Center, "Decay", ""}, 0.5f, 5.0, 25.0, "ms"},
+		WhiteMomentary7mm{{to_mm<96>(38.56), to_mm<96>(299.037), Center, "Trigger", ""}},
 		AnalogJackInput4ms{{to_mm<96>(38.56), to_mm<96>(134.369), Center, "Pitch CV In", ""}},
 		AnalogJackInput4ms{{to_mm<96>(38.56), to_mm<96>(262.876), Center, "Decay CV In", ""}},
 		GateJackInput4ms{{to_mm<96>(38.56), to_mm<96>(341.757), Center, "Trigger In", ""}},
@@ -26,9 +26,9 @@ struct ClaveInfo : ModuleInfoBase {
 }};
 
     enum class Elem {
-        TriggerButton,
         PitchKnob,
         DecayKnob,
+        TriggerButton,
         PitchCvIn,
         DecayCvIn,
         TriggerIn,
